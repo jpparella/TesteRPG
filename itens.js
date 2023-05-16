75540f409b559fc8e30f30666becc06f25c4709c
joao <joao.pparella@sp.senac.br>
2023-05-16 19:34:17 -0300

    Alterações teste Conflito:x

--- itens.js
+++ itens.js
@@ -6,7 +6,7 @@ export default class Itens{
 
     usar(entidade){
         if (entidade.nome != undefined){
-            entidade.fncES.mensagemCompleta(entidade.nome + " usou o "+this.nome+" e nada aconteceu!"); 
+            entidade.fncES.mensagemCompleta(entidade.nome + " usou o "+this.nome+" e nada aconteceu!!!!"); 
 
         }
     }    
